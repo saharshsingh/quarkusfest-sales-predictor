4c91b883cbd1073d9deecf710e8510ede8592899
Saharsh Singh <sahsingh@redhat.com>
2020-04-08 17:39:41 -0500

    Updated PmmlTest to do all items

--- sales-forecast-api/src/main/java/com/redhat/PmmlTest.java
+++ sales-forecast-api/src/main/java/com/redhat/PmmlTest.java
@@ -2,10 +2,10 @@ package com.redhat;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXBException;
 
@@ -21,52 +21,53 @@ import org.xml.sax.SAXException;
 
 public class PmmlTest {
 
-    public static void main(String[] args) throws IOException, SAXException, JAXBException {
-        // Building a model evaluator from a PMML file
-        File file = new File("Walmart.pmml");
-        Evaluator evaluator = new LoadingModelEvaluatorBuilder().load(file).build();
+	public static void main(String[] args) throws IOException, SAXException, JAXBException {
 
-        // Performing the self-check
-        evaluator.verify();
+		// record results here
+		final long start = System.nanoTime();
 
-        // Printing input (x1, x2, .., xn) fields
-        List<? extends InputField> inputFields = evaluator.getInputFields();
-        System.out.println("Input fields: " + inputFields);
+		// Building a model evaluator from a PMML file
+		final File file = new File("Walmart.pmml");
+		final Evaluator evaluator = new LoadingModelEvaluatorBuilder().load(file).build();
 
-        // Printing primary result (y) field(s)
-        List<? extends TargetField> targetFields = evaluator.getTargetFields();
-        System.out.println("Target field(s): " + targetFields);
+		// Performing the self-check
+		evaluator.verify();
 
-        // Printing secondary result (eg. probability(y), decision(y)) fields
-        List<? extends OutputField> outputFields = evaluator.getOutputFields();
-        System.out.println("Output fields: " + outputFields);
+		// Printing input (x1, x2, .., xn) fields
+		final Map<String, ? extends InputField> inputFields = evaluator.getInputFields().stream()
+				.collect(Collectors.toMap(i -> i.getName().getValue(), i -> i));
+		System.out.println("Input fields: " + inputFields);
 
-        // Reading a record from the data source
-        Map<String, String> inputRecord = new HashMap<>();
-        inputRecord.put("date", "101");
-        inputRecord.put("store_nbr", "1");
-        inputRecord.put("item_nbr", "9");
-        
-        // Mapping the record field-by-field from data source schema to PMML schema
-        Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
-        for (InputField inputField : inputFields) {
-            FieldName inputName = inputField.getName();
-            Object rawValue = inputRecord.get(inputName.getValue());
+		// Printing primary result (y) field(s)
+		final Map<String, ? extends TargetField> targetFields = evaluator.getTargetFields().stream()
+				.collect(Collectors.toMap(i -> i.getName().getValue(), i -> i));
+		System.out.println("Target field(s): " + targetFields);
 
-            // Transforming an arbitrary user-supplied value to a known-good PMML value
-            FieldValue inputValue = inputField.prepare(rawValue);
+		// Printing secondary result (eg. probability(y), decision(y)) fields
+		final List<? extends OutputField> outputFields = evaluator.getOutputFields();
+		System.out.println("Output fields: " + outputFields);
 
-            arguments.put(inputName, inputValue);
-        }
+		for (int i = 1; i <= 111; i++) {
+			final Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
 
-        // Evaluating the model with known-good arguments
-        Map<FieldName, ?> results = evaluator.evaluate(arguments);
+			final InputField date = inputFields.get("date");
+			arguments.put(date.getName(), date.prepare("101"));
 
-        // Decoupling results from the JPMML-Evaluator runtime environment
-        Map<String, ?> resultRecord = EvaluatorUtil.decodeAll(results);
+			final InputField store = inputFields.get("store_nbr");
+			arguments.put(store.getName(), store.prepare("1"));
 
-        // Print result
-        System.out.println(resultRecord);
-    }
+			final InputField item = inputFields.get("item_nbr");
+			arguments.put(item.getName(), item.prepare(i));
+
+			final Map<String, ?> result = EvaluatorUtil.decodeAll(evaluator.evaluate(arguments));
+			final Object units = result.get("units");
+			System.out.println(i+"="+units);
+		}
+
+		// Print result
+		final long stop = System.nanoTime();
+		System.out.println("Nanos: " + (stop - start));
+
+	}
 
 }
